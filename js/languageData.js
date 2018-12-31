da84c08c7bc98eb6f3d1b6ffaaa2262f51cb695e
kancheng <zxdfgcv@gmail.com>
2018-12-31 08:33:51 +0800

    Upload language change JS

---
+++ js/languageData.js
@@ -0,0 +1,53 @@
+﻿/* All Language Data */
+
+
+/* English */
+L_english = new Array(); 
+L_english[0] = "Horst Kan";
+L_english[1] = "Education";
+L_english[2] = "Experience";
+L_english[3] = "Skills";
+L_english[4] = "Project";
+L_english[5] = "Language";
+L_english[6] = "English";
+L_english[7] = "Traditional Chinese";
+L_english[8] = "Simplified Chinese";
+
+L_english[9] = "Horst Kan"; 
+L_english[10] = "Coding, R, Python, PHP and JavaScript are my passions. <br/>They are hoped to be my job in the future. <br/>Graduated from the Department of Information Management,<br/> Lunghwa University of Science and Technolog.<br/>I intend to be a Data Analyst and Web Developer.<br/><br/>";
+L_english[11] = "Coding, R, Python, PHP and JavaScript are my passions. <br/>They are hoped to be my job in the future. <br/>Graduated from the Department of Information Management,<br/> Lunghwa University of Science and Technolog.<br/>I intend to be a Data Analyst and Web Developer.<br/>";
+
+
+
+/* Traditional Chinese - Taiwan */
+L_taiwan = new Array(); 
+L_taiwan[0] = "干皓丞";
+L_taiwan[1] = "學歷";
+L_taiwan[2] = "經歷";
+L_taiwan[3] = "技能";
+L_taiwan[4] = "專案";
+L_taiwan[5] = "語言";
+L_taiwan[6] = "英文";
+L_taiwan[7] = "繁體中文";
+L_taiwan[8] = "簡體中文";
+
+L_taiwan[9] = "干皓丞"; 
+L_taiwan[10] = "畢業於龍華科技大學資訊管理研究所，<br />目前主要語言為 R 、 Python 、 PHP 和 JS，<br />未來會繼續作為工作來努力，<br />期望自己將來成為優異的資料分析師與 Web 開發者。<br /><br />";
+L_taiwan[11] = "畢業於龍華科技大學資訊管理研究所，<br />目前主要語言為 R 、 Python 、 PHP 和 JS，<br />未來會繼續作為工作來努力，<br />期望自己將來成為優異的資料分析師與 Web 開發者。<br />";
+
+
+/* Simplified Chinese */
+L_china = new Array();
+L_china[0] = "干皓丞";
+L_china[1] = "学历";
+L_china[2] = "经历";
+L_china[3] = "技能";
+L_china[4] = "专案";
+L_china[5] = "语言";
+L_china[6] = "英文";
+L_china[7] = "繁体中文";
+L_china[8] = "简体中文";
+
+L_china[9] = "干皓丞"; 
+L_china[10] = "毕业于龙华科技大学资讯管理研究所，<br />目前主要语言为R 、 Python 、 PHP 和JS，<br />未来会继续作为工作来努力，<br />期望自己将来成为优异的资料分析师与Web 开发者。 <br /><br />";
+L_china[11] = "毕业于龙华科技大学资讯管理研究所，<br />目前主要语言为R 、 Python 、 PHP 和JS，<br />未来会继续作为工作来努力，<br />期望自己将来成为优异的资料分析师与Web 开发者。 <br />";
