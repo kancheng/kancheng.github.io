0184179c1e7e70d39fbbf057970feae182fdf50c
kancheng <zxdfgcv@gmail.com>
2019-01-25 11:33:32 +0800

    edit

--- js/languageData.js
+++ js/languageData.js
@@ -23,6 +23,10 @@ L_english[14] = "Skills";
 L_english[15] = "Project";
 
 
+L_english[16] = "This research had constructed an interactive system of academic achievement analysis (A3S) with R Shiny technique which includes easy used functions of data preparation, data demographics, clustering analysis validation, analysis of variances and data visualization for teachers. This study also had completed the works of data understanding, data managing and data defining for eight-year scores data of compulsory subjects of all departments in the Business School based on the standard process for big data analysis. Finally, a case study on the academic achievements of the students enrolled in 2009 of the Department of Information Management was demonstrated for the system advantage.";
+L_english[17] = "The A3S system lets the users determine the parameters of cluster analysis by choosing the necessary subjects. When dividing the subjects into three categories: measurement, management and profession, choosing the subjects of calculation, economic and programming design as three indicators of academic achievements of the freshmen and using the condition of applying student loans as economic indicator, the case study of the Department of Information Management has found that the cluster analysis can forecast the performance of senior courses. Therefore, this system will aim at choosing their own useful subjects as effective indicators of cluster analysis for each department, help the teachers to discover the learning performance of students, to design the appropriate teaching facilities, teaching goals and methods, and to meet the requirements of students’ academic achievements.";
+
+
 /* Traditional Chinese - Taiwan */
 L_taiwan = new Array(); 
 L_taiwan[0] = "干皓丞";
@@ -45,6 +49,10 @@ L_taiwan[14] = "技能";
 L_taiwan[15] = "專案";
 
 
+L_taiwan[16] = "本研究使用 R Shiny 工具建立一個互動式操作的學業成就分析系統 (Academic achievement analysis system, A3S)，開發方便使用者進行資料準備、統計描述、集群分析驗證、變異數分析及視覺化顯示等互動操作之功能；也依據大數據分析流程，將管理學院各系歷年必修課學業成績進行資料理解、清理與定義；同時以資管系 2009 年註冊學生之學業成績進行個案分析研究。";
+L_taiwan[17] = "本系統可以動態選擇做為分群依據的科目，個案分析研究發現若將資管系科目分為三個種類:計量、管理與專業，以大一時期基礎的微積分、經濟學與程式設計三個科目做為學術能力指標，學生是否申請就學貸條件做為經濟指標，分群結果可預測學生在高年級不同專業領域代表科目的學業成績表現趨勢。因此本研究成果可幫助各個科系發展各自的有效分群指標，瞭解學生的學習狀況來發展合宜的教學設計、教學目標和方法以回應學生之學習需求。";
+
+
 /* Simplified Chinese */
 L_china = new Array();
 L_china[0] = "干皓丞";
@@ -65,6 +73,10 @@ L_china[12] = "学历";
 L_china[13] = "经历";
 L_china[14] = "技能";
 L_china[15] = "专案";
+
+L_china[16] = "本研究使用R Shiny 工具建立一个互动式操作的学业成就分析系统(Academic achievement analysis system, A3S)，开发方便使用者进行资料准备、统计描述、集群分析验证、变异数分析及视觉化显示等互动操作之功能；也依据大数据分析流程，将管理学院各系历年必修课学业成绩进行资料理解、清理与定义；同时以资管系2009年注册学生之学业成绩进行个案分析研究。";
+L_china[17] = "本系統可以動態選擇做為分群依據的科目，個案分析研究發現若將資管系科目分為三個種類:計量、管理與專業，以大一時期基礎的微積分、經濟學與程式設計三個科目做為學術能力指標，學生是否申請就學貸條件做為經濟指標，分群結果可預測學生在高年級不同專業領域代表科目的學業成績表現趨勢。因此，本研究成果可幫助各個科系發展各自的有效分群指標，瞭解學生的學習狀況，發展合宜的教學設計、教學目標和方法以回應學生之學習需求。";
+
 
 
 
